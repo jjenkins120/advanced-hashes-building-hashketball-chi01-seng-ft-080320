90f50acc0b2c606be93613cb258ec326ab63158d
Jeff Jenkins <jjenkins120@gmail.com>
2020-06-25 02:59:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,41 @@
 # Write your code here!
+def game_hash
+  
+end
+
+
+=begin
+{
+  :home=>{
+    :team_name=>"", 
+    :colors=>["",""], 
+    :players=>[
+      {:player_name=>""},
+      {:number=>},
+      {:shoe=>},
+      {:points=>},
+      {:rebounds=>},
+      {:assists=>},
+      {:steals=>},
+      {:blocks=>},
+      {:slam_dunks=>},
+      ]
+  }, 
+  
+  :away=>{
+    :team_name=>"", 
+    :colors=>["",""], 
+    :players=>[
+      {:player_name=>""},
+      {:number=>},
+      {:shoe=>},
+      {:points=>},
+      {:rebounds=>},
+      {:assists=>},
+      {:steals=>},
+      {:blocks=>},
+      {:slam_dunks=>},
+      ]
+  }
+}
+=end
